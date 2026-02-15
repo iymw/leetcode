b50edf982b3c27684c47ef8aa08dbf91326c6ee7
Michael Wayne <michaelwaynewibisono@gmail.com>
2026-02-15 22:33:22 +0700

    Time: 6 ms (25.26%), Space: 59.7 MB (66.59%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+    res = [];
+
+    function dfs(i, cur, total) {
+        if (total === target) {
+            res.push([...cur]);
+            return;
+        }
+
+        if (i >= candidates.length || total > target) {
+            return;
+        }
+
+        cur.push(candidates[i]);
+        dfs(i, cur, total + candidates[i]);
+        cur.pop();
+        dfs(i + 1, cur, total);
+    }
+
+    dfs(0, [], 0);
+    return res;
+};
