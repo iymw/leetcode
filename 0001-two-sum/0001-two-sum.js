9590a53b04931449df8505afdab13f172f1b7264
Michael Wayne <michaelwaynewibisono@gmail.com>
2026-02-28 12:44:45 +0700

    Time: 4 ms (52.64%), Space: 57.7 MB (6.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,20 +6,23 @@
 var twoSum = function (nums, target) {
     let left = 0, right = nums.length - 1, temp = 0, res = [];
     const sorted = [...nums].sort(function (a, b) { return a - b });
-    function findRight(indexLeft, sortedRight) {
+    function findAllIndex(left, right) {
         for (let i = 0; i < nums.length; i++) {
-            if (sortedRight === nums[i] && indexLeft !== i) {
-                return i;
+            if (left === nums[i]) {
+                res.push(i);
+                continue;
+            }
+
+            if (right === nums[i]) {
+                res.push(i);
             }
         }
+        return res;
     }
     while (left <= right) {
         temp = sorted[left] + sorted[right];
         if (target === temp) {
-            const indexLeft = nums.indexOf(sorted[left]);
-            res.push(indexLeft);
-            res.push(findRight(indexLeft, sorted[right]));
-            return res;
+            return findAllIndex(sorted[left], sorted[right]);
         }
 
         if (target < temp) {
